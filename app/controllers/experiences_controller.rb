ef6647a6f0d7d11e1281a4b025137bad65e22f05
Viktor Tagil <viktor.tagil@gmail.com>
2022-03-02 15:40:49 +0100

    added create action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,12 @@
 class ExperiencesController < ApplicationController
+  def create
+    @experience = Experience.new(experience_params)
+    @experience.save
+    redirect_to experience_path
+  end
+
+  private
+  def experience_params
+    params.require(:experience).permit(:name, :address, :busyness, :event_type)
+  end
 end
