d4fe30c9787e6ebd4f549fcdc6244f790175b566
Viktor Tagil <viktor.tagil@gmail.com>
2022-03-03 17:52:13 +0100

    adding again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :experience_users
+  has_many :experience_users, dependent: :destroy
   has_many :experiences, through: :experience_users
   has_one_attached :photo
   has_many :chatrooms, through: :chatroom_users
