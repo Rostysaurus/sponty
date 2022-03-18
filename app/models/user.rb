81d443e2adae2c11f8d324daca05d17caac00ae8
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2022-03-18 15:59:38 +0100

    sign up fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :experience_users
   has_many :experiences, through: :experience_users
-  has_one_attached :photo
-  has_many :chatrooms, through: :chatroom_users
   has_many :messages
+  has_many :chatrooms, through: :chatroom_users
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
 end
