c66d96e27afce75757fe3bd909177f22ead4bda6
rostyslav kanibolotskyi <rkanibolotskyi@gmail.com>
2022-04-03 17:38:56 +0200

    unit test to validate Experience creation

--- test/models/experience_test.rb
+++ test/models/experience_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ExperienceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "experience cannot be created without a name, event_type and address" do
+    experience = Experience.new(name: "Walhalla")
+    assert_not experience.save
+  end
 end
